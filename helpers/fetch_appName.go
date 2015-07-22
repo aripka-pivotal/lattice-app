299d6709778ae4fa18f1ef95c1c883f4d431f672
Andrew Ripka <aripka@pivotal.io>
2015-07-22 16:52:45 -0500

    pull app name from lattice PROCESS_GUID

--- helpers/fetch_appName.go
+++ helpers/fetch_appName.go
@@ -7,7 +7,12 @@ import(
 func FetchAppName() string {
         appName := os.Getenv("APP_NAME")
         if appName == "" {
-                return "Lattice-app"
+
+		//get the app name from lattice name
+		appName = os.Getenv("PROCESS_GUID")
+		if appName == "" {
+                	return "Lattice-app"
+		}
         }
         return appName
 }
