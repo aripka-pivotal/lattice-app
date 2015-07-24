9a80b68eb65365be0a1da8457dbe840bbb360f26
Andrew Ripka <aripka@pivotal.io>
2015-07-23 22:26:08 -0500

    add color index lookup

--- helpers/fetch_colors.go
+++ helpers/fetch_colors.go
@@ -11,6 +11,10 @@ const defaultColor = "008bb9"
 
 var adjustments = [][]int64 {{0,0,0},{0,12,13},{36,45,50}}
 
+var predefinedColors = []string {"FF00FF","FF0000","8B4513","FF8C00", "A9A9A9",
+	"FFFF00", "4B0082", "006400","808000","000000","800000",
+	"0000FF", "00008B","B8860B", "ADFF2F", "B0C4DE", "FF1493"}
+
 var colorHexStrings []string
 
 func init(){
@@ -18,8 +22,21 @@ func init(){
 	colorString := os.Getenv("HEX_COLOR")
 
         if colorString == "" {
-		fmt.Println("No color set using default")
-                colorString = defaultColor
+		colorString = os.Getenv("COLOR_INDX")
+		if colorString != "" {
+			colorIndex, indexErr := strconv.ParseInt(colorString,10,0) 
+			if indexErr != nil || colorIndex > int64(len(predefinedColors))  {
+				fmt.Println("Invalid color index using default")
+				colorString = defaultColor
+			}else if colorIndex == 0 {
+				colorString = defaultColor
+			}else{
+				colorString = predefinedColors[colorIndex-1]
+			}
+		}else{
+			fmt.Println("No color set using default")
+                	colorString = defaultColor
+		}
         }else if strings.ToLower(colorString) == "ffffff"{
 		fmt.Println("No color set to white not valid using default")
                 colorString = defaultColor
